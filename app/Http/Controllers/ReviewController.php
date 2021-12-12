b48a1d4836fa64802157202894aa80d9423a8837
khmish <30586743+khmish@users.noreply.github.com>
2021-12-12 17:13:25 +0300

    Update ReviewController.php

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -35,7 +35,7 @@ class ReviewController extends Controller
      */
     public function store(ReviewStoreRequest $request)
     {
-        $hasReview=Review::where('teacher_id',$request->teacher_id)->where('student_id',$request->student_id)->firstOrFail();
+        $hasReview=Review::where('teacher_id',$request->teacher_id)->where('student_id',$request->student_id)->first();
         if($hasReview){
 
             $review = Review::create($request->validated());
